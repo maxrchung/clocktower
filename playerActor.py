caebfaba5de4b6ed0d5b58a0bf006d2346865a70
Ted Won <tkwon09@gmail.com>
2015-04-12 10:28:27 -0700

    Removed HEAD stuff from playerActor

--- playerActor.py
+++ playerActor.py
@@ -1,8 +1,7 @@
 import pygame
 import actor
 import InputManager
-import soundManager
-
+import physicsManager
 from pygame.locals import *
  
 class PlayerActor(actor.Actor):
@@ -14,7 +13,6 @@ class PlayerActor(actor.Actor):
         self.orientation = "right"
         # create an input manager
         self.input = InputManager.InputManager()
-        self.sound = soundManager.SoundManager()
         
     def update(self):
         # update the input manager
@@ -31,7 +29,6 @@ class PlayerActor(actor.Actor):
                 self.currState = "JUMP_LEFT"
             else:
                 self.currState = "MOVE_LEFT"
-                self.sound.pauseMusic()
         elif self.input.R_DOWN:
             self.orientation = "right"
             if self.input.SPACE_DOWN:
@@ -42,26 +39,20 @@ class PlayerActor(actor.Actor):
             self.currState = "JUMP_NEUTRAL"
         else:
             self.currState = "IDLE"
-        print(self.currState)
 
     def checkState(self):
         if self.currState == "MOVE_LEFT":
-            self.accels['move'] = 4.0
-            self.targetVelocities['move'] = actor.vector.Vector(-12.0, None)
+            self.accels['move'] = 12.0
+            self.targetVelocities['move'] = actor.vector.Vector(-5.0, None)
         elif self.currState == "MOVE_RIGHT":
-            self.accels['move'] = 4.0
-            self.targetVelocities['move'] = actor.vector.Vector(12.0, None)
+            self.accels['move'] = 12.0
+            self.targetVelocities['move'] = actor.vector.Vector(5.0, None)
         elif self.currState == "JUMP_NEUTRAL":
-<<<<<<< HEAD
-            self.accels['jump'] = 4.0
-            self.targetVelocities['jump'] = actor.vector.Vector(0.0, 12.0)
-=======
-            self.accels['jump'] = 20.0
-            self.targetVelocities['jump'] = actor.vector.Vector(None, 24.0)
->>>>>>> 3a1543ae49c373d93a847d60690e90b768435e93
+            self.accels['jump'] = 200.0
+            self.targetVelocities['jump'] = actor.vector.Vector(None, 5.0)
         elif self.currState == "IDLE":
-            self.accels['move'] = 0.0
-            self.targetVelocities['move'] = actor.vector.Vector(0.0, 0.0)
+            self.accels['move'] = 20.0
+            self.targetVelocities['move'] = actor.vector.Vector(0.0, None)
             if 'jump' in self.accels:
                 self.accels.pop('jump')
                 self.targetVelocities.pop('jump')            
