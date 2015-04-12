ac9368170b63c6fb2dbe77323499dbd27485235b
Iryna Sulimova <isulimov@uci.edu>
2015-04-11 19:50:13 -0700

    Made it so the gearActor doesn't disintegrate into a vortex

--- gearActor.py
+++ gearActor.py
@@ -6,15 +6,21 @@ class GearActor(actor.Actor):
     def __init__(self, pos, spritePath, useGravity, groups):
         # Call parent constructor
         actor.Actor.__init__(self, pos, spritePath, useGravity, groups)
+        self.original_image = pygame.Surface(self.image)
+        self.current_angle = 0
 
     def rotateGear(self):
-        ANGLE = 10
+        OFFSET = 10
 
-        orig_rect = self.rect
-        rotated_image = pygame.transform.rotate(self.image, ANGLE)
-        rotated_rect = orig_rect.copy()
-        rotated_rect.center = rotated_image.get_rect().center
-        rotated_image = rotated_image.subsurface(rotated_rect).copy()
+        if self.current_angle < 360:
+            orig_rect = self.rect
+            rotated_image = pygame.transform.rotate(self.original_image, OFFSET)
+            rotated_rect = orig_rect.copy()
+            rotated_rect.center = rotated_image.get_rect().center
+            rotated_image = rotated_image.subsurface(rotated_rect).copy()
 
-        self.image = rotated_image
-        self.mask = pygame.mask.from_surface(self.image)
+            self.image = rotated_image
+            self.mask = pygame.mask.from_surface(self.image)
+            self.current_angle += OFFSET
+        else:
+            self.image = self.original_image
