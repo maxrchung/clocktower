df83b72804dbab763b32e62703147528ad775372
Dustin <dustinw3@gmail.com>
2015-04-11 19:20:38 -0700

    Updated the tile editor with actual images, starting to create a loader to parse through level files.

---
+++ Level Editor/tile_loader.py
@@ -0,0 +1,17 @@
+__author__:"Dustin"
+import pygame
+import tile_editor
+import sys
+import os
+
+class Level:
+	def __init__(self, screen, matrix):
+		self.window = screen
+		self.matrix = tiles
+
+	def parser(self):
+		for row in self.matrix:
+			for col in row:
+				if col == "X":
+					pass
+				elif col == ""
