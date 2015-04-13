321bcdfb5a72f8c0c7be14c3e3e06a80a9b4490d
Ted Won <tkwon09@gmail.com>
2015-04-12 17:13:32 -0700

    changed physics to use radius

--- physicsManager.py
+++ physicsManager.py
@@ -8,26 +8,32 @@ def checkCollisionAgainstGroup(actor, group):
     # check preliminary collision against group. get list of colliding sprites.
     collision = pygame.sprite.spritecollide(actor, group, False)
     # if collision occurred, do a mask check against the sprites it collided with
-    maskCollided = {}
+    circleCollided = []
     for collider in collision:
         # if there was a collision point on the mask, put it in the return list
-        collisionPoint = pygame.sprite.collide_mask(collider, actor)
-        if collisionPoint:
-            maskCollided[collider] = collisionPoint
-    return maskCollided
+        if pygame.sprite.collide_circle(actor, collider):
+            circleCollided.append(collider)
+    return circleCollided
 
-def resolveIntersection(actor, other):
-    # default to moving the actor up to resolve intersections
-    increment = vector.Vector(0.0, 1.0)
-    # if the actor has some velocity, move along the reverse velocity until it's
-    # no longer intersecting with the other collider
-    if actor.velocity.mag > 0.0:
-        # normalized reverse of actor's velocity
-        increment = actor.velocity.reverse().get_norm()
-    # otherwise, check if the actor's mask is below the other's. If so, move down.
-    elif actor.mask.centroid()[1] < other.mask.centroid()[1]:
-        increment = vector.Vector(0.0, -1.0)
-        
-    # while actor's mask is inside other's mask, move it until it's not intersecting anymore
-    while pygame.sprite.collide_mask(actor, other):
-        actor.moveActor(increment.x, increment.y)
+def resolveIntersection(actor, colliders):
+    # get the direction to move the actor out of the colliders
+    increment = vector.Vector(0.0, 0.0)
+    actorVec = vector.Vector(actor.center[0], actor.center[1])
+    for collider in colliders:
+        colliderVec = vector.Vector(collider.center[0], collider.center[1])
+        increment += actorVec - colliderVec
+    increment = increment.get_norm()
+    # while the actor is colliding, move it until it isn't colliding
+    if increment.mag == 0:
+        increment = vector.Vector(0.0, 1.0)
+    colliding = True
+    while colliding:
+        stillColliding = False
+        for collider in colliders:
+            if pygame.sprite.collide_circle(actor, collider):
+                stillColliding = True
+        if stillColliding:
+            actor.moveActor(increment.x, increment.y)
+        else:
+            colliding = False
+    
